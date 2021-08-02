35956e96fd5e517c17472f2a14532989509a36e5
Daniel Zamanillo <dzamanillok@gmail.com>
2021-08-02 10:37:01 -0500

    projects built

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { prompt } = require("inquirer");
 const inquirer = require("inquirer");
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js");
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
 // const pageHTML = generatePage(name, github);
 
@@ -154,5 +154,12 @@ const promptProject = (portfolioData) => {
 promptUser()
 	.then(promptProject)
 	.then((portfolioData) => {
-		console.log(portfolioData);
+		const pageHTML = generatePage(portfolioData);
+
+		fs.writeFile("index.html", pageHTML, (err) => {
+			if (err) throw err;
+			console.log(
+				"Portfolio complete! Check out index.html to see the output!"
+			);
+		});
 	});
